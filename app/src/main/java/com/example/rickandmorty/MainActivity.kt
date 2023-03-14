11a7e38ae2f3308d4233cf89a5d936e78fdb42e7
Tunahan Karyagdi <thankaryagdi@gmail.com>
2023-03-14 14:52:02 +0300

    folder structure created

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -17,8 +17,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+
             RickAndMortyTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
